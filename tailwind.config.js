50c0f4326eaa1cb092207b076a15bec8f56324cd
Ayessa <ayessamaedeguzman78@gmail.com>
2025-01-03 10:28:24 +0800

    friend.html finished and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2lg": "1100px",
+      },
+    },
   },
   plugins: [],
-}
+};
