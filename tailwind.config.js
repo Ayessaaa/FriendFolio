092c7d70e995aa955bd88294adc45b8f3a7cdbf1
Ayessa <ayessamaedeguzman78@gmail.com>
2025-01-05 23:44:52 +0800

    fixed bugs for empty db outputs

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,9 @@ module.exports = {
     },
   },
   safelist: [
-    { pattern: /^bg-(red|green|yellow|indigo|orange|brown|pink|zinc|violet|gray|sky|teal)-(100|200)$/ },
-    { pattern: /^text-(red|green|yellow|indigo|orange|brown|pink|zinc|violet|gray|sky|teal)-(400)$/ },
-    { pattern: /^border-(red|green|yellow|indigo|orange|brown|pink|zinc|violet|gray|sky|teal)-(200)$/ },
+    { pattern: /^bg-(red|green|yellow|indigo|orange|amber|pink|purple|violet|blue|sky|teal)-(100|200)$/ },
+    { pattern: /^text-(red|green|yellow|indigo|orange|amber|pink|purple|violet|blue|sky|teal)-(400)$/ },
+    { pattern: /^border-(red|green|yellow|indigo|orange|amber|pink|purple|violet|blue|sky|teal)-(200)$/ },
   ],
   plugins: [],
 };
